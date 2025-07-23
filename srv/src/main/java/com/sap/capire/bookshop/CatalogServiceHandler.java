bb1666ae4994a4c5821cd61b12e63348104cc04f
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-23 16:41:06 +0400

    refactor CatalogServiceHandler to use a constant for discount message

--- srv/src/main/java/com/sap/capire/bookshop/CatalogServiceHandler.java
+++ srv/src/main/java/com/sap/capire/bookshop/CatalogServiceHandler.java
@@ -13,11 +13,14 @@ import cds.gen.catalogservice.CatalogService_;
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
+
+  private static final String DISCOUNT_MESSAGE = " -- 11% discount!";
+
   @After(event = CqnService.EVENT_READ, entity = Books_.CDS_NAME)
   public void addDiscountIfApplicable(List<Books> books) {
     for (Books book : books) {
       if (book.getStock() != null && book.getStock() > 20) {
-        book.setTitle(book.getTitle() + " -- 11% discount!");
+        book.setTitle(book.getTitle() + DISCOUNT_MESSAGE);
       }
     }
   }
